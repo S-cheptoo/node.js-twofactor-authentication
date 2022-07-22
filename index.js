f6d122be56c3ff88f52a2bbeeb8d2736beca610c
Sandra <cheptoosandra.t@gmail.com>
2022-07-22 12:39:33 +0300

    register and temp secret

--- index.js
+++ index.js
@@ -11,7 +11,23 @@ const db = new JsonDB(new Config('myDatabase', true, false, '/'))
 
 app.get('/api', (req, res) => res.json({
     message: ' Welcome to the two factor authentication'
-}));
+}))
+
+//Register user & create temp secret
+app.get('/api/register', (req, res)=> {
+    const id = uuid.v4()
+
+    try {
+        const path = `/user/${id}`
+        const temp_secret = speakeasy.generateSecret()
+        db.push(path, { id, temp_secret })
+        res.json({ id, secret:temp_secret })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Error generating secret' })
+
+    }
+})
 
 const PORT = process.env.PORT || 5000
 
