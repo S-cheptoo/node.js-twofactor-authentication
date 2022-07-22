cde07b23798ed857af0dbd06f196c53c1f765c47
Sandra <cheptoosandra.t@gmail.com>
2022-07-22 13:25:31 +0300

    Verify Token and making secret permanent

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { Config } = ('node-json-db/dist/lib/JsonDBConfig');
 
 const app = express()
 
+app.use(express.json())
+
 //initialize the db
 const db = new JsonDB(new Config('myDatabase', true, false, '/'))
 
@@ -21,13 +23,40 @@ app.get('/api/register', (req, res)=> {
         const path = `/user/${id}`
         const temp_secret = speakeasy.generateSecret()
         db.push(path, { id, temp_secret })
-        res.json({ id, secret:temp_secret })
+        res.json({ id, secret:temp_secret.base32})
     } catch (error) {
         console.log(error)
         res.status(500).json({ message: 'Error generating secret' })
-
     }
 })
+
+//Verify Token and make secret permanent
+app.post('/api/verify',(req, res) =>{
+    const {token, userId} = req.body
+
+    try{
+        const path = `/user/${userId}`
+        const user = db.getData(path)
+
+        const {base32:secret} = user.temp_secret
+
+        const verified = speakeasy.totp.verify({ secret,
+            encoding: 'base32',
+            token
+        });
+
+        if(verified) {
+            db.push(path, { id: userId, secret: user.temp_secret} )
+            res.json({ verified: true})
+        } else {
+            res.json({verified: false})
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Error finding user'})
+    }
+})
+
 
 const PORT = process.env.PORT || 5000
 
